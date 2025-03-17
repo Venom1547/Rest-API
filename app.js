bffa00a14a58278d0c6d6ef7aac6aac5dd8a36f7
Venom1547 <b22ch014@iitj.ac.in>
2025-03-17 23:37:20 +0530

    Added CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors=require('cors');
 const app = express();
 const userRouter = require("./api/users/user.router");
 
 app.use(express.json());
 app.use("/api/users", userRouter);
+app.use(cors());
 
 const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
